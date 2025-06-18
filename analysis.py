b5f4bc8b14b302ef8f31bcda9477ee14353e9bd9
kadealdrich <kadealdrich@gmail.com>
2025-06-18 16:21:40 +0200

    plot initial gamma initial rho

--- analysis.py
+++ analysis.py
@@ -21,7 +21,7 @@ plt.scatter(gvr_filt['gamma_init'], gvr_filt['rho_final'], marker = 'o')
 plt.xlabel('Initial gamma')
 plt.ylabel('Final rho')
 plt.title('Initial gamma vs Final rho\n(Ignoring rho < -2)')
-plt.show()
+#plt.show()
 # seems like weird stuff really only happens when starting gamma near zero
 
 # gamma final vs rho final
@@ -30,7 +30,14 @@ plt.scatter(gvr_filt['gamma_final'], gvr_filt['rho_final'], marker = 'o')
 plt.xlabel('Final gamma')
 plt.ylabel('Final rho')
 plt.title('Final gamma vs Final rho\n(Ignoring rho < -2)')
-plt.show()
+#plt.show()
 # looks pretty identical to plot with initial gamma
 #   going to look at plot of differences of gamma vs differences of rho 
 
+# gamma init vs rho init 
+plt.figure(figsize = (8,5))
+plt.scatter(gvr['gamma_init'], gvr['rho_init'], marker = 'o')
+plt.xlabel('Initial gamma')
+plt.ylabel('Initial rho')
+plt.title('Initial gamma vs Inital rho')
+plt.show()
