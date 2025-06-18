375af8114fe2e2c38df312640b526ba3be7551c4
kadealdrich <kadealdrich@gmail.com>
2025-06-18 16:14:10 +0200

    add initial gamma vs final rho plot

--- analysis.py
+++ analysis.py
@@ -1,2 +1,24 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np 
+
 ### File for analyizing data related to experiments with kernel flow algorithm
+
+gvr = pd.read_csv("gamma-vs-rho.csv")
+
+# variables: gamma_init, gamma_final, rho_init, rho_final
+
+# removing crazy outliers
+gvr_filt = gvr[gvr['rho_final'] >= -2]
+
+## PLOTS
+
+# gamma init vs rho final
+plt.figure(figsize = (8,5))
+plt.scatter(gvr_filt['gamma_init'], gvr_filt['rho_final'], marker = 'o')
+plt.xlabel('Initial gamma')
+plt.ylabel('Final rho')
+plt.title('Initial gamma vs Final rho\n(Ignoring rho < -2)')
+plt.show()
+# seems like weird stuff really only happens when starting gamma near zero
 
