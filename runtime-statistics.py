3886b6ba4fe77441348d7f93e9a5717b413c5f25
kadealdrich <kadealdrich@gmail.com>
2025-07-20 15:53:51 +0200

    exploratory and statistical analysis of KRR algorithm run time differences between manual method and scipy

--- runtime-statistics.py
+++ runtime-statistics.py
@@ -5,7 +5,8 @@
 
 import pandas as pd
 import numpy as np 
-from scipy.stats import f, wilcoxon, ttest_rel 
+from scipy.stats import f, ttest_rel, kstest
+import matplotlib.pyplot as plt
 
 
 # load csv's of runtimes
@@ -36,7 +37,7 @@ print(f"Between  {SS_between:.4e}   {df_between}   {MS_between:.4e}   {F_val:.4f
 print(f"Within   {SS_within:.4e}   {df_within}   {MS_within:.4e}")
 print(f"Total    {(SS_between+SS_within):.4e}   {df_between+df_within}")
 
-### Wilcoxon signed-rank test
+
 ## loading in the different pairs of run times
 
 # with jit
@@ -51,7 +52,88 @@ sklearn_nojit = pd.read_csv("sklearn_krr_runtimes_noJIT.csv", header=None).iloc[
 manual_nojit_genmatrix  = pd.read_csv("manual_krr_runtimes_noJIT_genmatrix.csv", header=None).iloc[:,0].values
 sklearn_nojit_genmatrix = pd.read_csv("sklearn_krr_runtimes_noJIT_genmatrix.csv", header=None).iloc[:,0].values
 
+
 ## calculating the statistics
+
+# checking if differences are normally distributed 
+# necessary for running paired ttest
+diffs_jit = manual_jit - sklearn_jit
+diffs_nojit = manual_nojit - sklearn_nojit
+diffs_nojit_genmatrix = manual_nojit_genmatrix - sklearn_nojit_genmatrix
+
+mu_jit = np.mean(diffs_jit)
+mu_nojit = np.mean(diffs_nojit)
+mu_nojit_genmatrix = np.mean(diffs_nojit_genmatrix)
+
+sig_jit = np.std(diffs_jit, ddof = 1) # sample standard deviation
+sig_nojit = np.std(diffs_nojit, ddof = 1)
+sig_nojit_genmatrix = np.std(diffs_nojit_genmatrix, ddof = 1)
+
+# calculating ks statistics for each of the differences 
+ks_stat_jit, p_value_ks_jit = kstest(diffs_jit, 'norm', args=(mu_jit, sig_jit))
+ks_stat_nojit, p_value_ks_nojit = kstest(diffs_nojit, 'norm', args=(mu_nojit, sig_nojit))
+ks_stat_nojit_genmatrix, p_value_ks_nojit_genmatrix = kstest(diffs_nojit_genmatrix, 'norm', args=(mu_nojit_genmatrix, sig_nojit_genmatrix))
+
+print(f"KS statistic results (JIT):  |  KS = {ks_stat_jit:.3f}  |  p = {p_value_ks_jit:.3e}") 
+print(f"KS statistic results (no JIT):  |  KS = {ks_stat_nojit:.3f}  |  p = {p_value_ks_nojit:.3e}") 
+print(f"KS statistic results (no JIT genmatrix):  |  KS = {ks_stat_nojit_genmatrix:.3f}  |  p = {p_value_ks_nojit_genmatrix:.3e}") 
+
+# diffs histograms
+
+plt.figure() #jit
+plt.hist(diffs_jit, bins='auto')
+plt.xlabel('diffs_jit')
+plt.ylabel('Frequency')
+plt.title('Histogram of diffs')
+plt.show()
+
+plt.figure() #nojit
+plt.hist(diffs_nojit, bins='auto')
+plt.xlabel('diffs_nojit')
+plt.ylabel('Frequency')
+plt.title('Histogram of diffs')
+plt.show()
+
+plt.figure() #nojit genmatrix
+plt.hist(diffs_nojit_genmatrix, bins='auto')
+plt.xlabel('diffs_nojit_genmatrix')
+plt.ylabel('Frequency')
+plt.title('Histogram of diffs')
+plt.show()
+
+
+# scatterplot of diffs
+# Generate an array of indices 0, 1, 2, …, len(diffs)-1
+indices = np.arange(len(diffs_jit))
+
+# jit
+plt.figure()
+plt.scatter(indices, diffs_jit, s=20, alpha=0.7)  # s=size of points, alpha=transparency
+plt.xlabel('Iteration')
+plt.ylabel('Milliseconds')
+plt.title('Scatter Plot of diffs_jit vs. Iteration')
+plt.grid(True)
+plt.show()
+
+# no jit
+plt.figure()
+plt.scatter(indices, diffs_nojit, s=20, alpha=0.7)  # s=size of points, alpha=transparency
+plt.xlabel('Iteration')
+plt.ylabel('Milliseconds')
+plt.title('Scatter Plot of diffs_nojit vs. Iteration')
+plt.grid(True)
+plt.show()
+
+# no jit general matrix
+plt.figure()
+plt.scatter(indices, diffs_nojit_genmatrix, s=20, alpha=0.7)  # s=size of points, alpha=transparency
+plt.xlabel('Iteration')
+plt.ylabel('Milliseconds')
+plt.title('Scatter Plot of diffs_nojit_genmatrix vs. Iteration')
+plt.grid(True)
+plt.show()
+
+
 # alternate hypothesis is that the distribution of manual KRR run times is smaller 
 # with JIT and nice matrix assumptions 
 paired_ttest_jit = ttest_rel(a = manual_jit, b = sklearn_jit,
